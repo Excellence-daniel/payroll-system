772c31ed576ecbd57aa24b11f0434498aa9d218b
Excellence Oyeniran <oyeniranexcellence>
2020-10-05 10:45:51 +0100

    added a download button

--- src/components/admin/employee-details.js
+++ src/components/admin/employee-details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Employees from "../../assets/svgs/teamwork.svg";
 import { EmptyDataList } from "../empty-list";
+import DownloadIcon from "../../assets/svgs/direct-download.svg";
 
 const EmployeeDetails = ({ employees }) => {
   return (
@@ -13,28 +14,38 @@ const EmployeeDetails = ({ employees }) => {
         Create New Employee
       </button>
       {employees && employees.length ? (
-        <table className="table">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Salary</th>
-              <th scope="col">Status</th>
-              <th scope="col">Contract Years</th>
-            </tr>
-          </thead>
-          <tbody>
-            {employees.map((employee, key) => (
-              <tr key={key}>
-                <th scope="row">{key + 1}</th>
-                <td>{employee.employeename}</td>
-                <td>${employee.employeesalary}</td>
-                <td>{employee.employeestatus}</td>
-                <td>{employee.employeecontract}</td>
+        <>
+          <button className="btn new-btn download-btn">
+            Download Sheet
+            <img
+              src={DownloadIcon}
+              className="download-class"
+              alt="downloadimg"
+            />
+          </button>
+          <table className="table">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Salary</th>
+                <th scope="col">Status</th>
+                <th scope="col">Contract Years</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {employees.map((employee, key) => (
+                <tr key={key}>
+                  <th scope="row">{key + 1}</th>
+                  <td>{employee.employeename}</td>
+                  <td>${employee.employeesalary}</td>
+                  <td>{employee.employeestatus}</td>
+                  <td>{employee.employeecontract}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       ) : (
         <EmptyDataList imgSrc={Employees} text={"No Employee Data"} />
       )}
--- src/components/admin/reports.js
+++ src/components/admin/reports.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReportSvg from "../../assets/svgs/report.svg";
+import DownloadIcon from "../../assets/svgs/direct-download.svg";
 import { EmptyDataList } from "../empty-list";
 
 let count = 0;
@@ -16,30 +17,40 @@ const Reports = ({ reports, fetchReports }) => {
         Create New Report
       </button>
       {reports && reports.length ? (
-        <table className="table">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Project Name</th>
-              <th scope="col">Project Status</th>
-              <th scope="col">Assigned To</th>
-              <th scope="col">Timeline (Weeks)</th>
-              <th scope="col">Approved</th>
-            </tr>
-          </thead>
-          <tbody>
-            {reports.map((report, key) => (
-              <tr key={key}>
-                <th scope="row">{key + 1}</th>
-                <td>{report.projectname}</td>
-                <td>{report.projectstatus}</td>
-                <td>{report.assignedto}</td>
-                <td>{report.timeline}</td>
-                <td>{`${report.approved}`}</td>
+        <>
+          <button className="btn new-btn download-btn">
+            Download Sheet
+            <img
+              src={DownloadIcon}
+              className="download-class"
+              alt="downloadimg"
+            />
+          </button>
+          <table className="table">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Project Name</th>
+                <th scope="col">Project Status</th>
+                <th scope="col">Assigned To</th>
+                <th scope="col">Timeline (Weeks)</th>
+                <th scope="col">Approved</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {reports.map((report, key) => (
+                <tr key={key}>
+                  <th scope="row">{key + 1}</th>
+                  <td>{report.projectname}</td>
+                  <td>{report.projectstatus}</td>
+                  <td>{report.assignedto}</td>
+                  <td>{report.timeline}</td>
+                  <td>{`${report.approved}`}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       ) : (
         <EmptyDataList imgSrc={ReportSvg} text={"No Report"} />
       )}
